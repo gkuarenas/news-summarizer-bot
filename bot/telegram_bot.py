b72fb0ff62b4efafc81d9e802b658a756d8d2fba
Guillaume <guillaume.arenas0712@gmail.com>
2026-04-13 14:24:14 +0800

    feat: sent a message on News Summarizer Bot on telegram_bot.py

---
+++ bot/telegram_bot.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+import logging
+import os
+import asyncio
+from telegram import Bot
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def send_message(text: str):
+    bot = Bot(token=TOKEN)
+    await bot.send_message(
+        chat_id=CHAT_ID, 
+        text=text
+    )
+
+if __name__ == '__main__':
+    asyncio.run(send_message("Hello!"))
